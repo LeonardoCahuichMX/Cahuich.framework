8301d2654b39c4df7e422b91c4ceab1290e7187c
LeonardoCahuichLopez <leonardo.cahuich@icloud.com>
2024-09-30 19:39:47 -0600

    Update framework.js

--- v0.0.1/js/framework.js
+++ v0.0.1/js/framework.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
     //Framework
+    function buscadorcreditos() {
+        $("*").attr('creditos', function() {
+            if($(this).attr('creditos') != undefined) {
+                console.log( $(this).attr('creditos') );
+
+                var idintenficador = 'data-info-'+$(this).attr('creditos-numero');
+                var coordenadas = $(this).position();
+                $(".creditos .data-creditos ul").prepend('<li><a href="#'+idintenficador+'">('+$(this).attr('creditos-numero')+')</a> '+$(this).attr('creditos')+' - <a href="'+$(this).attr('creditos-link')+'">'+$(this).attr('creditos-link')+'</a></li>');
+                var altocontentThis = $(this).outerHeight() + coordenadas.top + 10;
+                $(".creditos-data-info-content").prepend('<div style="position: absolute;top: '+altocontentThis+'px;left: '+coordenadas.left+'px;" class="creditos-data-info">('+$(this).attr('creditos-numero')+') <a href="#creditos">Datos en la seccion de creditos</a></div>');
+                $(this).attr('id', idintenficador);
+            }
+        })
+    }
+    buscadorcreditos();
 });
 
 $(window).scroll(function(){
