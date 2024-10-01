87e2847a7d0cdf9af31a1035545ed1344a248380
LeonardoCahuichLopez <leonardo.cahuich@icloud.com>
2024-09-30 20:23:31 -0600

    Update framework.js

--- v0.0.1/js/framework.js
+++ v0.0.1/js/framework.js
@@ -174,7 +174,7 @@ $(document).ready(function () {
             }
             //$(this).css( "background-color", "red" );
         })
-    }
+    };
     /*alert("hola encendido");*/
     buscadorcreditos();
     observadoranimaciones();
