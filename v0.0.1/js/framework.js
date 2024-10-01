28d3c128bc0fb65696ebd66a5af95e116b16376e
LeonardoCahuichLopez <leonardo.cahuich@icloud.com>
2024-09-30 19:35:36 -0600

    ya

--- v0.0.1/js/framework.js
+++ v0.0.1/js/framework.js
@@ -1,55 +1,6 @@
 $(document).ready(function () {
-/* ##################################################
-
-    Nav
-
-##################################################### */
-
-    $(window).scroll(function(){
-        if($(this).scrollTop() <= $(".contenido").offset().top){
-            $('.nav-fixed-body .top_nav .nav').removeClass('nav_fixed');
-        }
-        if($(this).scrollTop() > $(".contenido").offset().top) {
-            $('.nav-fixed-body .top_nav .nav').addClass('nav_fixed');
-        }
-    });
-const body = document.body;
-const nav = document.querySelector(".top_nav");
-const scrollUp = "scroll-up";
-const scrollDown = "scroll-down";
-let lastScroll = 0;
-
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll == 0) {
-    body.classList.remove(scrollUp);
-    return;
-  }
-  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-    // down
-    body.classList.remove(scrollUp);
-    body.classList.add(scrollDown);
-  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
-    // up
-    body.classList.remove(scrollDown);
-    body.classList.add(scrollUp);
-  }
-  lastScroll = currentScroll;
+    //Framework
 });
-
-/* ##################################################
-
-    Nav-2
-
-##################################################### */
 
 $(window).scroll(function(){
-    if($(this).scrollTop() <= $(".contenido").offset().top){
-        $('.nav-fixed-body .top_nav-2 .nav').removeClass('nav_fixed-2');
-    }
-    if($(this).scrollTop() > $(".contenido").offset().top) {
-        $('.nav-fixed-body .top_nav-2 .nav').addClass('nav_fixed-2');
-    }
-});
-
 });
